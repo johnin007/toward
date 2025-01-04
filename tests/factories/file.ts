484186a2aae64fadb6ee832f3acbf14ec5a25073
johnin007 <johnsmithin007@yahoo.com>
2025-01-04 11:53:48 +0000

    Document tests - improving performance

--- tests/factories/file.ts
+++ tests/factories/file.ts
@@ -1,6 +1,6 @@
 import { File } from "../../src/searchee";
 
-export const fileFactory = (overrides: Partial<File> = {}): File => {
+export let fileFactory = (overrides: Partial<File> = {}): File => {
 	return {
 		name: "media.mp4",
 		length: 0,
